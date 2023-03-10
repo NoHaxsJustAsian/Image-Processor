c4311d1f29e6cbab9433c10573d7ce83a2d865a6
Win Tongtawee <WinTongtawee@gmail.com>
2023-03-10 16:19:41 -0500

    Added To-Do list

--- src/model/Filter.java
+++ src/model/Filter.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Filter implements IFilter{
+public class Filter implements IFilter {
+  private Layer layer;
+  private String command;
+
+  public Filter(Layer layer, String command) {
+    this.layer = layer;
+    this.command = command;
+  }
+
+  public Layer getLayer() {
+    return this.layer;
+  }
+
+  public String getCommand() {
+    return this.command;
+  }
+
+  public void applyBrightenLuma(int value) {
+    
+  }
 }
--- src/model/Layer.java
+++ src/model/Layer.java
@@ -2,6 +2,10 @@ package model;
 
 /**
  * Represents a Layer on the Image.
+ * A layer can accept an image and location, in which, the image is placed onto the layer's canvas
+ * at said location.
+ * The canvas, now holds each pixel from the image on its on canvas, and can be fed more images.
+ * If a pixel is overridden on the canvas, it will show the last placed image's pixel.
  */
 public class Layer {
   String name;
@@ -38,3 +42,17 @@ public class Layer {
 
 
 }
+
+
+
+
+//To-Do List.
+//constructor that takes in the canvas size (from initial project size), and the name of the layer.
+//make array of pixels that is the canvas of the layer (make it a 2d array) (the size of project given by user)
+//
+//make methods that take in images, and their location (offset), and apply it to the canvas of the layer
+// (make sure to check if the image is within the bounds of the canvas) (this will have to iterate thought
+// the image's arraylist, and then apply the offsets to add to the arraylist index, to apply the shift.
+//make method that applies layer.
+
+
