d9044399b9f0431ca34efca5b09945652ce37028
Win Tongtawee <WinTongtawee@gmail.com>
2023-03-14 12:19:12 -0400

    Added To-Do list

--- src/model/IPixel.java
+++ src/model/IPixel.java
@@ -1,10 +1,31 @@
 package model;
 
+/**
+ * Represents an interface for Pixel.
+ */
 public interface IPixel {
-
+  /**
+   * Retrieves the Red value of the Pixel.
+   * @return int red value of the pixel.
+   */
   public int getRed();
+
+  /**
+   * Retrieves the Blue value of the Pixel.
+   * @return int blue value of the pixel.
+   */
   public int getBlue();
+
+  /**
+   * Retrieves the Green value of the Pixel.
+   * @return int green value of the pixel.
+   */
   public int getGreen();
+
+  /**
+   * Retrieves the Alpha value of the Pixel.
+   * @return int alpha value of the pixel.
+   */
   public int getAlpha();
 
 }
