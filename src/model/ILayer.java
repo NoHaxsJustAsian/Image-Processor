cd79b82160ec95479787528430fadba0331aef8b
Win Tongtawee <WinTongtawee@gmail.com>
2023-03-15 23:31:04 -0400

    fixed minor type errors + added test directory

--- src/model/ILayer.java
+++ src/model/ILayer.java
@@ -9,33 +9,37 @@ public interface ILayer {
 
   /**
    * Sets the filter for the layer.
+   *
    * @param filter to be set.
    */
-  public void setFilter(IFilter filter);
+  void setFilter(IFilter filter);
 
   /**
    * Adds an image to the layer.
-   * @param x offset
-   * @param y offset
+   *
+   * @param x     offset
+   * @param y     offset
    * @param image to be added.
    */
-  public void addImage(IImage image,int x, int y);
+  void addImage(IImage image, int x, int y);
 
 
   /**
    * Removes an image from the layer
+   *
    * @param image to be removed.
    */
-  public void removeImage(IImage image);
+  void removeImage(IImage image);
 
 
   /**
    * This method returns the canvas from the layer.
    */
-  public IPixel[][] getCanvas();
+  IPixel[][] getCanvas();
 
   /**
    * Returns the name of the layer.
+   *
    * @return String name.
    */
   String getName();
