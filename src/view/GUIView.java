38c3c57e44a4284b2515d15550a8d49aeb9d845b
Drtygor <kyeung114@gmail.com>
2023-04-12 19:01:41 -0400

    Update GUIView.java

--- src/view/GUIView.java
+++ src/view/GUIView.java
@@ -285,6 +285,8 @@ public class GUIView extends JFrame implements IImageProcessorView {
     //LAYER PANEL
     layerPane.add(layerList);
     layerPane.add(this.formats);
+    layerPane.add(addLayerButton);
+    layerPane.add(selectLayerButton);
 
 
     this.pack();
@@ -426,8 +428,8 @@ public class GUIView extends JFrame implements IImageProcessorView {
 
     addLayerButton.addActionListener(e -> {
       f.addLayer(this.addLayerHelp());
-
     });
+
     addImageButton.addActionListener(e -> f.addImage(curLayer));
   }
 
