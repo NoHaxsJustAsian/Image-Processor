09b9f567546ed4b9615de80c4260216c5ddd5b6f
Drtygor <kyeung114@gmail.com>
2023-04-20 22:59:53 -0400

    dwa

--- src/controller/IImageProcessorController.java
+++ src/controller/IImageProcessorController.java
@@ -1,7 +1,9 @@
 package controller;
 
 /**
- * Represents the controller interface for this program. This interface will be used to start the
+ * Represents the controller interface for this program. It includes methods for starting the
+ * program, reading commands from an input source, and executing those commands using a model
+ * and a view.
  */
 public interface IImageProcessorController {
 
